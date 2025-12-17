29f0f6720f242fe49187228aeb6698175c3a383d
zinwhite1420 <zinwhite1420@gmail.com>
2025-12-17 15:17:10 +0900

    feat(common-jpa): add BaseSoftDeleteEntity

---
+++ common-jpa/src/main/java/com/malleo/common/jpa/entity/BaseSoftDeleteEntity.java
@@ -0,0 +1,34 @@
+package com.malleo.common.jpa.entity;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@MappedSuperclass
+@Getter
+public abstract class BaseSoftDeleteEntity extends BaseEntity {
+
+	@Getter
+	@Column(name = "deleted", nullable = false)
+	private boolean deleted = false;
+
+	@Column(name = "deleted_at")
+	private Instant deletedAt;
+
+	public void markDeleted() {
+		this.deleted = true;
+		this.deletedAt = Instant.now();
+	}
+}
+
+/*
+서비스 사용 예시
+@Entity
+@SQLDelete(sql = "UPDATE users SET deleted = true, deleted_at = now() WHERE id = ?")
+@SQLRestriction("deleted = false")
+public class User extends SoftDeleteEntity {
+    // ...
+}
+ */
