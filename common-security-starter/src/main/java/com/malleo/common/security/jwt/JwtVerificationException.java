24ec4c748a6dcaf1a0a3b29a5be08ed1eea232a5
zinwhite1420 <zinwhite1420@gmail.com>
2025-12-19 16:48:13 +0900

    feat: add common security starter with configurable properties and tests

--- common-security-starter/src/main/java/com/malleo/common/security/jwt/JwtVerificationException.java
+++ common-security-starter/src/main/java/com/malleo/common/security/jwt/JwtVerificationException.java
@@ -1,7 +1,7 @@
 package com.malleo.common.security.jwt;
 
 public class JwtVerificationException extends RuntimeException {
-
+	@SuppressWarnings("unused")
 	public JwtVerificationException(String message) {
 		super(message);
 	}
